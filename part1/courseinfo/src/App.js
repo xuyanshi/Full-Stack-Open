f9eed1718a0820eb268b79b5080854e14f24c488
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 02:16:07 +0800

    Update App.js

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,19 +1,24 @@
 function App() {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <h1>{course}</h1>
+            <p>
+                {part1} {exercises1}
+            </p>
+            <p>
+                {part2} {exercises2}
+            </p>
+            <p>
+                {part3} {exercises3}
+            </p>
+            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
         </div>
     );
 }
