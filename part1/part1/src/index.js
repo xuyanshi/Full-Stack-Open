da8b849189cb73bdc287844ec96d0457fc47c08b
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 00:27:51 +0800

    Update index.js

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
