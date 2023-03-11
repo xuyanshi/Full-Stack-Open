5922e354163afdf44c2077fed15485c1d5dee6bd
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 19:18:37 +0800

    Update App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -8,15 +8,15 @@ const App = () => {
 
   const handleLeftClick = () => {
     const newClicks = {
-      left: clicks.left + 1,
-      right: clicks.right
+      ...clicks,
+      left:clicks.left+1
     }
     setClicks(newClicks)
   }
 
   const handleRightClick = () => {
     const newClicks = {
-      left: clicks.left,
+      ...clicks,
       right: clicks.right + 1
     }
     setClicks(newClicks)
