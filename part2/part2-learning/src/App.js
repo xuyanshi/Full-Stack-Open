48a87061d26ccc8cfb8cb4fcb7e29fa5e80e50db
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-12 00:21:57 +0800

    Update App.js

--- part2/part2-learning/src/App.js
+++ part2/part2-learning/src/App.js
@@ -5,7 +5,11 @@ const App = (props) => {
         <div>
             <h1>Notes</h1>
             <ul>
-                {notes.map(note => <li>{note.content}</li>)}
+                {notes.map(note =>
+                    <li>
+                        {note.content}
+                    </li>
+                )}
             </ul>
         </div>
     )
