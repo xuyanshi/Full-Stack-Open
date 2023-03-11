29f4badf868158de0ad8f8bbb372ac3ecd3551f2
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-12 02:30:24 +0800

    Update App.js

--- part2/part2-learning/src/App.js
+++ part2/part2-learning/src/App.js
@@ -11,6 +11,10 @@ const App = (props) => {
         console.log('button clicked', event.target)
     }
 
+    const handleNoteChange = (event) => {
+        console.log(event.target.value)
+        setNewNote(event.target.value)
+    }
     return (
         <div>
             <h1>Notes</h1>
@@ -20,7 +24,7 @@ const App = (props) => {
                 )}
             </ul>
             <form onSubmit={addNote}>
-                <input value={newNote}/>
+                <input value={newNote} onChange={handleNoteChange}/>
                 <button type="submit">save</button>
             </form>
         </div>
