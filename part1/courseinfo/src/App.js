784107ba8be9aa202044cf313405589e1cc99afa
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 13:48:53 +0800

    Update App.js

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -31,25 +31,23 @@ const Total = (props) => {
 
 function App() {
     const course = 'Half Stack application development'
-    const part1 = {
+    const parts = [{
         name: 'Fundamentals of React',
         exercises: 10
-    }
-    const part2 = {
+    }, {
         name: 'Using props to pass data',
         exercises: 7
-    }
-    const part3 = {
+    }, {
         name: 'State of a component',
         exercises: 14
-    }
+    }]
     return (
         <div>
             <Header course={course}/>
-            <Content part1={part1.name} part2={part2.name} part3={part3.name} exercises1={part1.exercises}
-                     exercises2={part2.exercises}
-                     exercises3={part3.exercises}/>
-            <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+            <Content part1={parts[0].name} part2={parts[1].name} part3={parts[2].name} exercises1={parts[0].exercises}
+                     exercises2={parts[1].exercises}
+                     exercises3={parts[2].exercises}/>
+            <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises}/>
         </div>
     );
 }
