72f43642b15e91b8736ba82e9dbdcc70d73cb240
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 19:31:44 +0800

    Update App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,26 +1,38 @@
 import {useState} from 'react'
 
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+}
+
 const App = () => {
 
-    const [clicks, setClicks] = useState({
-        left: 0, right: 0
-    })
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
 
     const [allClicks, setAll] = useState([])
 
-    const handleLeftClick = () =>
-        setClicks({...clicks, left: clicks.left + 1})
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
 
-
-    const handleRightClick = () =>
-        setClicks({...clicks, right: clicks.right + 1})
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
 
     return (
         <div>
-            {clicks.left}
+            {left}
             <button onClick={handleLeftClick}>left</button>
             <button onClick={handleRightClick}>right</button>
-            {clicks.right}
+            {right}
             <p>{allClicks.join(' ')}</p>
         </div>
     )
