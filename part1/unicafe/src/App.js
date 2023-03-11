6d62e34f5822b7dda2ab416ba8b030592d0df85c
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 20:47:46 +0800

    Update App.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,14 @@
 import {useState} from 'react'
 
+const Buttom = (props) => {
+
+}
+
+const Feedback = (props) => {
+    return (
+        <p>{props.rank} {props.cnt}</p>
+    )
+}
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0)
@@ -9,6 +18,9 @@ const App = () => {
     return (
         <div>
             <h1>give feedback</h1>
+            <h2>statistics</h2>
+            <Feedback rank={'good'} cnt={good}/>
+            
         </div>
     )
 }
