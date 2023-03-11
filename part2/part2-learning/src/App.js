7d44a76c9fba482aadf56461b1afbbd7f940f6a4
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-12 02:23:07 +0800

    Update App.js

--- part2/part2-learning/src/App.js
+++ part2/part2-learning/src/App.js
@@ -3,16 +3,24 @@ import {useState} from "react";
 
 const App = (props) => {
     const [notes, setNotes] = useState(props.notes)
+
+    const addNote = (event) => {
+        event.preventDefault()
+        console.log('button clicked', event.target)
+    }
+
     return (
         <div>
             <h1>Notes</h1>
             <ul>
-                <ul>
-                    {notes.map(note =>
-                        <Note key={note.id} note={note}/>
-                    )}
-                </ul>
+                {notes.map(note =>
+                    <Note key={note.id} note={note}/>
+                )}
             </ul>
+            <form onSubmit={addNote}>
+                <input/>
+                <button type="submit">save</button>
+            </form>
         </div>
     )
 }
