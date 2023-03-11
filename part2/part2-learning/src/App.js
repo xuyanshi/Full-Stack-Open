733bb6cb78506422d999d7c80f4e772c623e8630
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-12 02:38:15 +0800

    Update App.js

--- part2/part2-learning/src/App.js
+++ part2/part2-learning/src/App.js
@@ -29,6 +29,11 @@ const App = (props) => {
     return (
         <div>
             <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'important' : 'all'}
+                </button>
+            </div>
             <ul>
                 {notesToShow.map(note =>
                     <Note key={note.id} note={note}/>
