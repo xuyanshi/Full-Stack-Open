f7b5dc22bdabfb2a4d0cdce779eb47728a8a89cc
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 17:00:04 +0800

    Update App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -3,10 +3,6 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  const handleClick = () => {
-    console.log("clicked")
-  }
-
   setTimeout(
     () => setCounter(counter + 1),
     1000
@@ -17,7 +13,7 @@ const App = () => {
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={handleClick}>
+      <button onClick={() => setCounter(counter+1)}>
         plus
       </button>
     </div>
