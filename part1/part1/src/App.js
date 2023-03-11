045a3d08e7a6b88b7c45dbad5c182e0787033bdd
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 16:21:57 +0800

    Update App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -23,6 +23,10 @@
 // }
 
 const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear
+    return yearNow-props.age
+  }
   return (
     <div>
       <p>Hello {props.name}, you're {props.age} years old.</p>
