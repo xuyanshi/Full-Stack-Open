ae74a40a99391e53c448ec69bc5c1ec368d84b33
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 16:42:57 +0800

    Update index.js

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -3,19 +3,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App counter={counter} />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
