07c188434e633c02da21b093108b5b394a0bb113
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 16:32:15 +0800

    Update App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,47 +1,8 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>Hello {name}, you're {age} years old.</p>
-      <p>You were probably born in {bornYear()}.</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => { 
-  const name = "Maitian"
-  const age = 24
-  return (
-  <>
-    <h1>Hello World</h1>
-    <Hello name="Emo" age="23" />
-    <Hello name={name} age={age} />
-  </>
-  )
-}
-
-export default App;
+export default App
