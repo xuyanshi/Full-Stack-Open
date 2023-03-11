317b105519bd698fe4e08efba7771859a4228187
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 16:43:28 +0800

    Update App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,5 +1,13 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
   return (
     <div>{counter}</div>
   )
