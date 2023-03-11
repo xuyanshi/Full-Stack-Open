0a0bcd9acbe89a7b8cc4092ade8848212d4c455e
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-12 01:53:22 +0800

    Update App.js

--- part2/coursedata/src/App.js
+++ part2/coursedata/src/App.js
@@ -23,39 +23,72 @@ const Content = ({parts}) =>
         />
     </>
 
-const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        },
-        {
-            name: 'Redux',
-            exercises: 11,
-            id: 4
-        }
-    ]
-
-    const total = parts.reduce((accumulator, currentValue) => {
+const Course = ({course}) => {
+    const total = course.parts.reduce((accumulator, currentValue) => {
         // console.log('what is happening', accumulator, currentValue)
         return accumulator + currentValue.exercises
     }, 0)
 
     return (
         <div>
-            <Header course={course}/>
-            <Content parts={parts}/>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
             <Total sum={total}/>
         </div>
+    )
+}
+
+const App = () => {
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+
+
+    return (
+        <>
+
+        </>
     )
 }
 
