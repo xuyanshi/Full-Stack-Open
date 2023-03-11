712d9b50bc7c79d6ca594f1cd3f4a560e7de2eff
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 19:25:54 +0800

    Update App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,39 +1,32 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 const App = () => {
 
-  const [clicks,setClicks] = useState({
-    left:0, right:0
-  })
+    const [clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
 
-  const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left:clicks.left+1
-    }
-    setClicks(newClicks)
-  }
+    const [allClicks, setAll] = useState([])
+    
+    const handleLeftClick = () =>
+        setClicks({...clicks, left: clicks.left + 1})
 
-  const handleRightClick = () => {
-    const newClicks = {
-      ...clicks,
-      right: clicks.right + 1
-    }
-    setClicks(newClicks)
-  }
 
-  return (
-    <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
-      {clicks.right}
-    </div>
-  )
+    const handleRightClick = () =>
+        setClicks({...clicks, right: clicks.right + 1})
+
+    return (
+        <div>
+            {clicks.left}
+            <button onClick={handleLeftClick}>
+                left
+            </button>
+            <button onClick={handleRightClick}>
+                right
+            </button>
+            {clicks.right}
+        </div>
+    )
 }
 
 export default App
