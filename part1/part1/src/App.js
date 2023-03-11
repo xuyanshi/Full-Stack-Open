1aa6f21019760119a52989f62dcdea76d85c3c79
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-11 17:28:33 +0800

    Update App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -2,13 +2,11 @@ import { useState } from 'react'
 
 const Display = ({counter}) => <div>{counter}</div>
 
-const Button = ({onClick, text}) => {
-  return (
+const Button = ({onClick, text}) => (
     <button onClick={onClick}>
       {text}
     </button>
   )
-}
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
