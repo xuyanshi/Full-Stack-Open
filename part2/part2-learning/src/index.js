a426e1e873028c348767ae945f9e28ba602d9863
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 01:51:47 +0800

    Update index.js

--- part2/part2-learning/src/index.js
+++ part2/part2-learning/src/index.js
@@ -6,7 +6,9 @@ import App from './App'
 import axios from 'axios'
 
 const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
+promise.then(response => {
+    console.log(response)
+})
 
 const promise2 = axios.get('http://localhost:3001/foobar')
 console.log(promise2)
