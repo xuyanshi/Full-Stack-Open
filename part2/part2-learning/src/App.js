4686901f0100aeb75327530f8461fa96040a0484
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 02:18:37 +0800

    Update App.js

--- part2/part2-learning/src/App.js
+++ part2/part2-learning/src/App.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react'
 import axios from 'axios'
+import Note from './components/Note'
 
 const App = () => {
     const [notes, setNotes] = useState([])
@@ -16,7 +17,46 @@ const App = () => {
             })
     }, [])
     console.log('render', notes.length, 'notes')
+    
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5,
+            id: notes.length + 1,
+        }
 
-    // ...
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+    }
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value)
+        setNewNote(event.target.value)
+    }
+
+    const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'important' : 'all'}
+                </button>
+            </div>
+            <ul>
+                {notesToShow.map(note =>
+                    <Note key={note.id} note={note}/>
+                )}
+            </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange}/>
+                <button type="submit">save</button>
+            </form>
+        </div>
+    )
 }
+
 export default App
