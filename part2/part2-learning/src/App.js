5580ace6b40d28c0c4e47da0c3f9fb4a2405d6ad
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 02:17:14 +0800

    Update App.js

--- part2/part2-learning/src/App.js
+++ part2/part2-learning/src/App.js
@@ -1,50 +1,22 @@
-import Note from './components/Note'
-import {useState} from "react";
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
-const App = (props) => {
-    const [notes, setNotes] = useState(props.notes)
-    const [newNote, setNewNote] = useState('a new note...')
+const App = () => {
+    const [notes, setNotes] = useState([])
+    const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
 
-    const addNote = (event) => {
-        event.preventDefault()
-        const noteObject = {
-            content: newNote,
-            date: new Date().toISOString(),
-            important: Math.random() < 0.5,
-            id: notes.length + 1,
-        }
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/notes')
+            .then(response => {
+                console.log('promise fulfilled')
+                setNotes(response.data)
+            })
+    }, [])
+    console.log('render', notes.length, 'notes')
 
-        setNotes(notes.concat(noteObject))
-        setNewNote('')
-    }
-
-    const handleNoteChange = (event) => {
-        console.log(event.target.value)
-        setNewNote(event.target.value)
-    }
-
-    const notesToShow = showAll ? notes : notes.filter(note => note.important)
-
-    return (
-        <div>
-            <h1>Notes</h1>
-            <div>
-                <button onClick={() => setShowAll(!showAll)}>
-                    show {showAll ? 'important' : 'all'}
-                </button>
-            </div>
-            <ul>
-                {notesToShow.map(note =>
-                    <Note key={note.id} note={note}/>
-                )}
-            </ul>
-            <form onSubmit={addNote}>
-                <input value={newNote} onChange={handleNoteChange}/>
-                <button type="submit">save</button>
-            </form>
-        </div>
-    )
+    // ...
 }
-
 export default App
