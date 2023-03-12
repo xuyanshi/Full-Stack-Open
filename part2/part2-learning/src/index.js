6807486ed7166959d4f5c694bd0cd4afae5c82cc
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 01:36:18 +0800

    Update index.js

--- part2/part2-learning/src/index.js
+++ part2/part2-learning/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/notes')
+console.log(promise)
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
 const notes = [
     {
         id: 1,
