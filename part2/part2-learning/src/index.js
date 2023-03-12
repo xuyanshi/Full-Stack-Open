12d4ce3a727a34a9fa1b477a210bd92338fe77c9
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 02:16:33 +0800

    Update index.js

--- part2/part2-learning/src/index.js
+++ part2/part2-learning/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import axios from 'axios'
 
 import App from './App'
 
-axios.get('http://localhost:3001/notes').then(response => {
-    const notes = response.data
-    ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes}/>)
-})
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
