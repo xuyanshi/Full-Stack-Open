b6249aa6ed03f883ec7e9425baf2de276673c7ea
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-12 11:48:06 +0800

    Update App.js

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,10 +8,14 @@ const Person = ({person}) => {
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', phone: '040-1234567'}
+        {name: 'Arto Hellas', number: '040-123456', id: 1},
+        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
+        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
     ])
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
+    const [newId, setNewId] = useState(persons.length + 1)
     const addNewPerson = (event) => {
         event.preventDefault()
         let findIdx = persons.findIndex((p) => {
@@ -20,13 +24,15 @@ const App = () => {
         if (findIdx === -1) {
             setPersons(persons.concat({
                 name: newName,
-                phone: newPhone
+                phone: newPhone,
+                id: newId
             }))
         } else {
             alert(`${newName} is already added to phonebook`)
         }
         setNewName('')
         setNewPhone('')
+        setNewId(newId + 1)
     }
 
     const handleNameChange = (event) => {
@@ -35,7 +41,7 @@ const App = () => {
     const handlePhoneChange = (event) => {
         setNewPhone(event.target.value)
     }
-    
+
     return (
         <div>
             <h2>Phonebook</h2>
@@ -51,8 +57,7 @@ const App = () => {
             <h2>Numbers</h2>
             <div>
                 {persons.map(person =>
-                    // Warning: Keys should be unique so that components maintain their identity across updates.
-                    <Person key={person} person={person}/>
+                    <Person key={person.id} person={person}/>
                 )}
             </div>
         </div>
