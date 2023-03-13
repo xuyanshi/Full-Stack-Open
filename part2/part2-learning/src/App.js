877da784d77c76f20a4f2871abd9bcc8a1563de2
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 21:13:20 +0800

    Update App.js

--- part2/part2-learning/src/App.js
+++ part2/part2-learning/src/App.js
@@ -7,6 +7,16 @@ const App = () => {
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
 
+    const toggleImportanceOf = id => {
+        const url = `http://localhost:3001/notes/${id}`
+        const note = notes.find(n => n.id === id)
+        const changedNote = {...note, important: !note.important}
+
+        axios.put(url, changedNote).then(response => {
+            setNotes(notes.map(note => note.id !== id ? note : response.data))
+        })
+    }
+
     useEffect(() => {
         axios
             .get('http://localhost:3001/notes')
@@ -48,11 +58,13 @@ const App = () => {
                 </button>
             </div>
             <ul>
-                <ul>
-                    {notesToShow.map(note =>
-                        <Note key={note.id} note={note}/>
-                    )}
-                </ul>
+                {notesToShow.map(note =>
+                    <Note
+                        key={note.id}
+                        note={note}
+                        toggleImportance={() => toggleImportanceOf(note.id)}
+                    />
+                )}
             </ul>
             <form onSubmit={addNote}>
                 <input value={newNote} onChange={handleNoteChange}/>
