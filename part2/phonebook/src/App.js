73a5d949ad14fdb8a5a910febb7ee9b1b88025ca
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 13:41:05 +0800

    2.11 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,25 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from "axios";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456', id: 1},
-        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-    ])
+    // const [persons, setPersons] = useState([
+    //     {name: 'Arto Hellas', number: '040-123456', id: 1},
+    //     {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+    //     {name: 'Dan Abramov', number: '12-43-234345', id: 3},
+    //     {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
+    // ])
+    const [persons, setPersons] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, [])
+
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
     const [newId, setNewId] = useState(persons.length + 1)
