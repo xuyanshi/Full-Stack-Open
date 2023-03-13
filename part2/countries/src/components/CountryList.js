67d03d7709f10578bfb07c619f8c8cc1a66a19f6
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 18:56:43 +0800

    Update CountryList.js

--- part2/countries/src/components/CountryList.js
+++ part2/countries/src/components/CountryList.js
@@ -1,24 +1,35 @@
 import CountryInfo from "./CountryInfo";
 import {useState} from "react";
+import countries from "./Countries";
 
 const CountryList = ({queryResult}) => {
     const [showInfo, setShowInfo] = useState(false)
 
     return (
         <>
-            <div>
-                {queryResult.map((country) => {
-                    return (
-                        <div>
-                            {country.name.common}
-                            <button onClick={() => setShowInfo(!showInfo)}>
-                                show
-                            </button>
-                        </div>
-                    )
-                })}
-            </div>
-
+            {showInfo &&
+                <div>
+                    {queryResult.map((country) => {
+                        return (
+                            <div>
+                                {country.name.common}
+                                <button onClick={() => {
+                                    setShowInfo(!showInfo)
+                                    //How can I clear this page and generate a new html page?
+                                }}>
+                                    show
+                                </button>
+                            </div>
+                        )
+                    })}
+                </div>
+            }
+            {!showInfo &&
+                <div>
+                    <CountryInfo country={queryResult[0]}/>
+                    {setShowInfo(!showInfo)}
+                </div>
+            }
         </>
     )
 }
