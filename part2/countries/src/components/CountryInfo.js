b2c9d9599b111e7e69e2b6593bfc3eed07efb0e1
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 16:01:49 +0800

    Update CountryInfo.js

--- part2/countries/src/components/CountryInfo.js
+++ part2/countries/src/components/CountryInfo.js
@@ -1,12 +1,28 @@
 const CountryInfo = ({queryResult}) => {
-    console.log(queryResult)
     const country = queryResult[0]
-    console.log(country)
+
+    let languages = []
+    const getLanguages = () => {
+        let countryLanguages = country.languages
+        for (const lang in countryLanguages) {
+            languages = languages.concat(countryLanguages[lang])
+        }
+    }
+    getLanguages()
+    
+    const getLang = () => {
+
+    }
     return (
         <div>
             <h2>{country.name.common}</h2>
             <div>capital {country.capital}</div>
             <div>area {country.area}</div>
+            <h3>languages: </h3>
+            <ul>
+                <li></li>
+            </ul>
+            <img src={country.flags.png}/>
         </div>
     )
 }
