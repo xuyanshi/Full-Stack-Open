5ac3ece5be63d1a23178044df831a289256da5bc
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 19:40:34 +0800

    Update CountryInfo.js

--- part2/countries/src/components/CountryInfo.js
+++ part2/countries/src/components/CountryInfo.js
@@ -1,6 +1,16 @@
 import Languages from "./Languages";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 const CountryInfo = ({country}) => {
+    const api_key = process.env.REACT_APP_API_KEY
+    const [weather, setWeather] = useState(null)
+    useEffect(() => {
+        axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API key}`)
+            .then(response => {
+                setWeather(response.data)
+            })
+    }, [])
     return (
         <div>
             <h2>{country.name.common}</h2>
@@ -10,7 +20,7 @@ const CountryInfo = ({country}) => {
             <Languages country={country}/>
             <img src={country.flags.png} alt={"flag of " + country.name.common}/>
             <h3>Weather in {country.capital}</h3>
-            
+
         </div>
     )
 }
