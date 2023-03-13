aefeba982154ec9151ed017ed1e96b2e0688c4af
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 19:54:11 +0800

    Update CountryInfo.js

--- part2/countries/src/components/CountryInfo.js
+++ part2/countries/src/components/CountryInfo.js
@@ -4,13 +4,28 @@ import {useEffect, useState} from "react";
 
 const CountryInfo = ({country}) => {
     const api_key = process.env.REACT_APP_API_KEY
-    const [weather, setWeather] = useState(null)
-    useEffect(() => {
-        axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API key}`)
-            .then(response => {
-                setWeather(response.data)
-            })
-    }, [])
+    const [weather, setWeather] = useState({
+        "coord": {"lon": -0.13, "lat": 51.51},
+        "weather": [{"id": 300, "main": "Drizzle", "description": "light intensity drizzle", "icon": "09d"}],
+        "base": "stations",
+        "main": {"temp": 280.32, "pressure": 1012, "humidity": 81, "temp_min": 279.15, "temp_max": 281.15},
+        "visibility": 10000,
+        "wind": {"speed": 4.1, "deg": 80},
+        "clouds": {"all": 90},
+        "dt": 1485789600,
+        "sys": {"type": 1, "id": 5091, "message": 0.0103, "country": "GB", "sunrise": 1485762037, "sunset": 1485794875},
+        "id": 2643743,
+        "name": "London",
+        "cod": 200
+    })
+
+    // useEffect(() => {
+    //     axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}&appid=${api_key}`)
+    //         .then(response => {
+    //             setWeather(response.data)
+    //         })
+    // }, [])
+
     return (
         <div>
             <h2>{country.name.common}</h2>
@@ -20,7 +35,9 @@ const CountryInfo = ({country}) => {
             <Languages country={country}/>
             <img src={country.flags.png} alt={"flag of " + country.name.common}/>
             <h3>Weather in {country.capital}</h3>
+            <div>Temperature {(weather.main.temp - 273.15).toFixed(2)} Celsius</div>
 
+            <div>wind {weather.wind.speed.toFixed(2)} m/s</div>
         </div>
     )
 }
