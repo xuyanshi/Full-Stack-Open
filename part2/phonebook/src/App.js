122e29287bbe505304bc22f0d1d40b3b3b1c19df
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-14 00:59:19 +0800

    2.15 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -36,12 +36,9 @@ const App = () => {
             }
             axios.post('http://localhost:3001/persons', personObject)
                 .then(response => {
-                    console.log(response)
+                    setPersons(persons.concat(response.data))
                 })
-            // setPersons(persons.concat({
-            //     name: newName,
-            //     phone: newPhone,
-            // }))
+
         } else {
             alert(`${newName} is already added to phonebook`)
         }
