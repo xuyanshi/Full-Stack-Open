caeb6f05d065676e223922ef7190161792c3af1f
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 13:51:47 +0800

    update

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ])
+    const [newName, setNewName] = useState('')
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form>
+                <div>
+                    name: <input/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            ...
+        </div>
+    )
 }
 
-export default App;
+export default App
