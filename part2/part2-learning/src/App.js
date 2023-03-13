a98b5a533133c68adeaef2d30f8f004eb4f7386e
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-13 21:03:52 +0800

    official codes

--- part2/part2-learning/src/App.js
+++ part2/part2-learning/src/App.js
@@ -8,38 +8,35 @@ const App = () => {
     const [showAll, setShowAll] = useState(true)
 
     useEffect(() => {
-        console.log('effect')
         axios
             .get('http://localhost:3001/notes')
             .then(response => {
-                console.log('promise fulfilled')
                 setNotes(response.data)
             })
     }, [])
-    console.log('render', notes.length, 'notes')
 
     const addNote = (event) => {
         event.preventDefault()
         const noteObject = {
             content: newNote,
-            date: new Date().toISOString(),
-            important: Math.random() < 0.5,
+            important: Math.random() > 0.5,
         }
+
         axios
             .post('http://localhost:3001/notes', noteObject)
             .then(response => {
-                setNotes(notes.concat(noteObject))
+                setNotes(notes.concat(response.data))
                 setNewNote('')
             })
-
     }
 
     const handleNoteChange = (event) => {
-        console.log(event.target.value)
         setNewNote(event.target.value)
     }
 
-    const notesToShow = showAll ? notes : notes.filter(note => note.important)
+    const notesToShow = showAll
+        ? notes
+        : notes.filter(note => note.important)
 
     return (
         <div>
@@ -50,9 +47,11 @@ const App = () => {
                 </button>
             </div>
             <ul>
-                {notesToShow.map(note =>
-                    <Note key={note.id} note={note}/>
-                )}
+                <ul>
+                    {notesToShow.map(note =>
+                        <Note key={note.id} note={note}/>
+                    )}
+                </ul>
             </ul>
             <form onSubmit={addNote}>
                 <input value={newNote} onChange={handleNoteChange}/>
--- part2/part2-learning/src/index.js
+++ part2/part2-learning/src/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+axios.get('http://localhost:3001/notes').then(response => {
+    const notes = response.data
+    ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes}/>)
+})
