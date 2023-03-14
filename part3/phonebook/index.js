6fced5902bb13c92a015fc6669d1d7da2a349b4d
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-14 16:12:56 +0800

    Update index.js

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -30,3 +30,8 @@ let persons = [
 app.get('/',(request,response) => {
     request.send('<h1>Home Page</h1>>')
 })
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
