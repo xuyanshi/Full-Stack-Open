e2fbb5962cfae9ac22cf06bb9e7ea80e471267df
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-14 14:19:31 +0800

    webstrom init

--- part3/part3-learning/index.js
+++ part3/part3-learning/index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.writeHead(200, {'Content-Type': 'text/plain'})
     response.end('Hello World')
 })
 
