76bea34d99bd8d09955eb978416abd9e4f828a36
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-14 15:01:24 +0800

    update

--- part3/part3-learning/index.js
+++ part3/part3-learning/index.js
@@ -33,7 +33,18 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
-    response.json(note)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
 })
 
 
