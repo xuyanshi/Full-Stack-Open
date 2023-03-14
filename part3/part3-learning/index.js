568c0712eac97520dbf6da30a4464adbd375dc5b
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-14 14:10:22 +0800

    Update index.js

--- part3/part3-learning/index.js
+++ part3/part3-learning/index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const http = require('http')
+
+const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.end('Hello World')
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
