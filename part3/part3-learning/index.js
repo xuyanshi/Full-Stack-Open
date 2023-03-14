e3dd1b04d189a3d6a4df10cb097052609295b102
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-14 14:27:19 +0800

    Update index.js

--- part3/part3-learning/index.js
+++ part3/part3-learning/index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const express = require('express')
 const app = express()
 
 let notes = [
@@ -21,6 +21,15 @@ let notes = [
         important: true
     }
 ]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
 
 const PORT = 3001
 app.listen(PORT)
