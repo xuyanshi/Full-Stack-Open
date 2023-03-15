9c4ee852ed3e8b3f1d67ebaa18e8215e40d55ccf
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-15 09:56:28 +0800

    change tiny

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,7 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 let persons = [
     {
