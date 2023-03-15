9ab811a815547b4a67057b98e443b5c831631f55
xuyanshi <xuyanshi1999@gmail.com>
2023-03-15 21:48:58 +0800

    Update index.js

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -56,13 +56,9 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(req.params.id).then(p => {
+        res.json(p)
+    })
 })
 
 const generateId = () => {
