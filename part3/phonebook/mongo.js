45bd7fa61966f6701f7a2d96975e0dc1b8298c92
xuyanshi <xuyanshi1999@gmail.com>
2023-03-15 17:42:02 +0800

    3.12 done

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,22 +1,9 @@
 const mongoose = require('mongoose')
 const argvLen = process.argv.length
 const password = process.argv[2]
+let newName = ''
+let newNumber = ''
 
-if (argvLen < 3) {
-    console.log('Please provide accurate arguments: node mongo.js <password> (<name> <phone-number>)')
-    process.exit(1)
-} else if (argvLen === 3) {
-
-} else if (argvLen === 5) {
-
-} else {
-    console.log('Please provide the password as an argument: node mongo.js <password> <name> <phone-number>')
-    process.exit(1)
-}
-
-
-const newName = process.argv[3]
-const newNumber = process.argv[4]
 
 const url =
     `mongodb+srv://xuyanshi1999:${password}@cluster-test.psvw21y.mongodb.net/phonebookApp?retryWrites=true&w=majority`
@@ -51,5 +38,13 @@ const fetchPerson = () => {
     })
 }
 
-createPerson()
-
+if (argvLen === 3) {
+    fetchPerson()
+} else if (argvLen === 5) {
+    newName = process.argv[3]
+    newNumber = process.argv[4]
+    createPerson()
+} else {
+    console.log('Please provide the password as an argument: node mongo.js <password> <name> <phone-number>')
+    process.exit(1)
+}
