183f5aed8dcb285df36031c43fa04a3dc9eb5e48
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-15 10:20:59 +0800

    Update index.js

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -90,7 +90,7 @@ app.delete('/api/persons:id', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
