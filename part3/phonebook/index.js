81f5bb27c61e7d8e19f71d93b4a4698092234fcd
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-16 10:13:48 +0800

    3.19 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -44,6 +44,8 @@ const errorHandler = (err, req, res, next) => {
     console.error(err.message)
     if (err.name === 'CastError') {
         return res.status(400).send({error: 'malformed id'})
+    } else if (err.name === 'ValidationError') {
+        return res.status(400).json({error: err.message})
     }
     next(err)
 }
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -10,8 +10,15 @@ mongoose.connect(url)
 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true
+    }
 })
 
 
