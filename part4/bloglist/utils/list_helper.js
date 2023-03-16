1657317cbf60a788a74ed8d21493e8e9cc975ff5
xuyanshi <xuyanshi1999@gmail.com>
2023-03-16 21:05:38 +0800

    totalLikes

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,7 +3,14 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    
+    if (blogs.length === 0) {
+        return 0;
+    }
+    let total = 0
+    for (const blog of blogs) {
+        total += blog.likes
+    }
+    return total
 }
 
 module.exports = {
