414efec0930991c5066216eba5ead2637dfe91da
xuyanshi <xuyanshi1999@gmail.com>
2023-03-16 16:22:20 +0800

    update

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,1 +1,14 @@
 const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+// get all blogs
+blogsRouter.get('/', (req, res) => {
+    Blog.find({}).then(b => {
+        res.json(b)
+    })
+})
+
+// get one blog
+blogsRouter.get('/:id', (req, res, next) => {
+    Blog.findById
+})
