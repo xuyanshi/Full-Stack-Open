ecdd01718abcf9c1d04d2f9e2b50b03755033af8
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-16 08:37:20 +0800

    Update index.js

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -45,6 +45,7 @@ const errorHandler = (err, req, res, next) => {
     if (err.name === 'CastError') {
         return res.status(400).send({error: 'malformed id'})
     }
+    next(err)
 }
 
 app.get('/', (request, response) => {
