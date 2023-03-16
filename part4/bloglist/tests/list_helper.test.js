aab2cb913f46d854c26bbf3112e3b301743174de
xuyanshi <xuyanshi1999@gmail.com>
2023-03-16 21:00:39 +0800

    test

---
+++ part4/bloglist/tests/list_helper.test.js
@@ -0,0 +1,7 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+    const blogs = []
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+})
