dcecd6ab9a6ff245231d40848b4e957b92f3f0e5
xuyanshi <xuyanshi1999@gmail.com>
2023-03-16 16:12:40 +0800

    update

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,6 +4,11 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+// const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
 const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
@@ -21,6 +26,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', (request, response) => {
+    logger.info('get all blogs...', new Date().toLocaleDateString(), new Date().toLocaleTimeString())
     Blog
         .find({})
         .then(blogs => {
@@ -29,8 +35,8 @@ app.get('/api/blogs', (request, response) => {
 })
 
 app.post('/api/blogs', (request, response) => {
+    logger.info('create a new blog...', new Date().toLocaleDateString(), new Date().toLocaleTimeString())
     const blog = new Blog(request.body)
-
     blog
         .save()
         .then(result => {
@@ -38,7 +44,6 @@ app.post('/api/blogs', (request, response) => {
         })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
