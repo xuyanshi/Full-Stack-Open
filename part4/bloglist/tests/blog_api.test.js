d24b14d9e225edd63400b93b11f77fa29af522b6
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-17 21:42:23 +0800

    update

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -116,6 +116,20 @@ test('create a new blog without url', async () => {
 
 }, 100000)
 
+test('delete a blog', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
