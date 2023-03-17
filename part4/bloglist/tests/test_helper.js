f703266897f687a723687070551437a6537bee8a
xuyanshi <xuyanshi1999@gmail.com>
2023-03-17 20:18:34 +0800

    Update test_helper.js

---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,18 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        "title": "Test Title",
+        "author": "xuyanshi",
+        "url": "https://xuyanshi.github.io/",
+        "likes": 12345,
+        "id": "6414468a31cc17f65a79f48c"
+    },
+    {
+        "title": "KMP algorithm",
+        "author": "K-M-P",
+        "url": "https://xuyanshi.github.io/",
+        "likes": 123456,
+        "id": "641448f7ee77b454be7d05fd"
+    }
+]
