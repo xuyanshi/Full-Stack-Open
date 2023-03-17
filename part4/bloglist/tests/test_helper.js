44e80acc8e452f9e24d0a72e34610f0eebd2343d
xuyanshi <xuyanshi1999@gmail.com>
2023-03-17 20:42:58 +0800

    nonExistingId

--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -21,3 +21,16 @@ const blogsInDb = async () => {
     const blogs = await Blog.find({})
     return blogs.map(blog => blog.toJSON())
 }
+const nonExistingId = async () => {
+    const blog = new Blog({
+        "title": "nonExisting",
+        "author": "nonExisting",
+        "url": "nonExisting",
+        "likes": 0
+    })
+
+    await blog.save()
+    await blog.remove()
+
+    return blog._id.toString()
+}
