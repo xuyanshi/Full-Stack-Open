c787cb219020f7edd2e8665879aadbf4e9fb068e
xuyanshi <xuyanshi1999@gmail.com>
2023-03-17 19:46:42 +0800

    update

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+
 
 test('blogs are returned as json', async () => {
     await api
