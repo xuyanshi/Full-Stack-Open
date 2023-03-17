643786d754fa579190559f42b7737099a96ed9b3
xuyanshi <xuyanshi1999@gmail.com>
2023-03-17 18:30:40 +0800

    tset

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
