0b2c8f43176b83f0154c41a9ba528e588a777f44
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 19:20:06 +0800

    Update blog_api.test.js

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -174,7 +174,28 @@ describe('when there is one user in database initially', () => {
         await user.save()
     })
 
-    text('creation succeeds with a fresh username', async () => {
+    test('creation fails with proper status code and message if username already exists', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Super User',
+            password: 'pswd'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('username must be unique')
+
+        const userAtEnd = await helper.usersInDb()
+        expect(userAtEnd).toEqual(usersAtStart)
+    })
+
+    test('creation succeeds with a fresh username', async () => {
         const userAtStart = await helper.usersInDb()
 
         const newUser = {
