39c3b94ff9d983ff4106abe1f3a9c8df31a43a1d
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 18:23:03 +0800

    controllers\users.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,12 +1,14 @@
 const config = require('./utils/config')
 const express = require('express')
-require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const blogRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+require('express-async-errors')
+
+const usersRouter = require('./controllers/users')
+const blogsRouter = require('./controllers/blogs')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -23,7 +25,7 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/blogs', blogRouter)
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
