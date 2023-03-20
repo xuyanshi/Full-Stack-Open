71b7726ce59f273e13c78d84cb605984909bcfd1
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 18:33:18 +0800

    app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -26,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
