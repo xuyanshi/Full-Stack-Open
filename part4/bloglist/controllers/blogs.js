78266f9ac7815ea8fdefd462d29445dcd5e198f8
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 21:35:40 +0800

    create a new blog with a token

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -36,7 +36,13 @@ const getTokenFrom = request => {
 blogsRouter.post('/', async (req, res) => {
     const body = req.body
 
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return res.status(401).json({error: 'token missing or invalid'})
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     if (body.likes === undefined) {
         body.likes = 0
