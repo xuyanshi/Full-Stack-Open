440c1725cac7fb88d73ea4942488600313c4b125
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 17:58:54 +0800

    userSchema

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    name: String,
+    passwordHash: String,
+    blogs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Blog'
+    }],
+})
