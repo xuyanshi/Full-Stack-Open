4f2c9ca2c6e9c4f3f413f4f59fa7c335252c883d
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 17:49:01 +0800

    update package

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -1,4 +1,4 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
