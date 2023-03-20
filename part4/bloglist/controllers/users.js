cde228b15b4f8323093cb7ef8132b87ddc97cde3
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 18:49:59 +0800

    userRouter

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcryptjs')
+const userRouter = require('express').Router()
+
+const User = require('../models/user')
+
+userRouter.post('/', async (req, res) => {
+    const {username, name, password} = req.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username,
+        name,
+        passwordHash
+    })
+
+    const savedUser = await user.save()
+
+    res.status(201).json(savedUser)
+})
+
+module.exports = userRouter
