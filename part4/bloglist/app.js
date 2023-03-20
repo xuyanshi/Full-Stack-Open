46ac3b326cb932dda32ab3e9c738d475cf910184
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 21:18:41 +0800

    4.18 done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -9,6 +9,7 @@ require('express-async-errors')
 
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -27,6 +28,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -3,4 +3,29 @@ const bcrypt = require('bcryptjs')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
 
-loginRouter()
+loginRouter.post('/', async (req, res) => {
+    const {username, password} = req.body
+
+    const user = await User.findOne({username})
+    const passwordCorrect = (user === null)
+        ? false
+        : await bcrypt.compare(password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+        return res.status(401).json({
+            error: 'invalid username or password'
+        })
+    }
+
+    const userForToken = {
+        username: user.username,
+        id: user._id
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    res.status(200)
+        .send({token, username: user.username, name: user.name})
+})
+
+module.exports = loginRouter
