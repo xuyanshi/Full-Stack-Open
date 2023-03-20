b4733533a47052b8a40fa8fbb107bea8238003d6
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 22:08:43 +0800

    4.19 done

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -19,7 +19,11 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({error: 'malformed id'})
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({error: error.message})
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({error: 'invalid token'})
     }
+
+    logger.error(error.message)
 
     next(error)
 }
