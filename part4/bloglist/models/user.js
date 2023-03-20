c35ee5917dd78794d58e6738f487a70f54cd9a9e
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 18:03:57 +0800

    userSchema toJSON

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -9,3 +9,13 @@ const userSchema = new mongoose.Schema({
         ref: 'Blog'
     }],
 })
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
