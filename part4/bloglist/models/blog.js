95a4867f9d3086e4db5075286fd31c537746d71f
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-20 18:18:47 +0800

    update blog.js

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -19,6 +19,10 @@ const blogSchema = new mongoose.Schema({
     likes: {
         type: Number,
         required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 })
 
