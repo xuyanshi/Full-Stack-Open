2f8b12f59e245a4f44eb323aafdb445aebb26d8f
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-21 15:35:49 +0800

    Update blogs.js

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
+
 const baseUrl = '/api/blogs'
 
+let token = null
+
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+export default {getAll}
