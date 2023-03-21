fa71021b402ab65ee23a5757cdaafb195728ff01
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-21 15:51:52 +0800

    Update App.js

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -26,6 +26,7 @@ const App = () => {
             const user = await loginService.login({
                 username, password
             })
+            blogService.setToken(user.token)
             setUser(user)
             setUsername('')
             setPassword('')
