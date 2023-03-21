aebe92fe7714abbbbe777ae2ff98974955f96c8b
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-21 15:02:22 +0800

    Update App.js

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -26,8 +26,26 @@ const App = () => {
         return (
             <div>
                 <h2>Log in to application</h2>
-                <form>
-                    //...
+                <form onSubmit={handleLogin}>
+                    <div>
+                        username
+                        <input
+                            type="text"
+                            value={username}
+                            name="Username"
+                            onChange={({target}) => setUsername(target.value)}
+                        />
+                    </div>
+                    <div>
+                        password
+                        <input
+                            type="password"
+                            value={password}
+                            name="Password"
+                            onChange={({target}) => setPassword(target.value)}
+                        />
+                    </div>
+                    <button type="submit">login</button>
                 </form>
             </div>
         )
