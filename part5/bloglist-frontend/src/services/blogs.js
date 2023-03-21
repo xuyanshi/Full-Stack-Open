1ea2d716afcf6fb716471b792ff76341e11bccce
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-21 15:36:40 +0800

    Update blogs.js

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -4,6 +4,10 @@ const baseUrl = '/api/blogs'
 
 let token = null
 
+const setToken = (newToken) => {
+    token = `bearer ${newToken}`
+}
+
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
