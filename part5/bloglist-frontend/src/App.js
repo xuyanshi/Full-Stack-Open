ebf37646680c69ae44149bbb2c433622ee37c566
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-21 16:39:16 +0800

    Update App.js

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -163,9 +163,15 @@ const App = () => {
         <div>
             <h2>blogs</h2>
             {blogForm()}
-            {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog}/>
-            )}
+            <ul>
+                {blogs.map(blog =>
+                    <li>
+                        <Blog key={blog.id} blog={blog}/>
+                    </li>
+                )}
+            </ul>
+
+            <Footer/>
         </div>
     )
 
