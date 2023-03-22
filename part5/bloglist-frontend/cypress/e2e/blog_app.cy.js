8a97c5aa24271f388b41edb213df711b3ea14cd3
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-22 23:14:35 +0800

    Update blog_app.cy.js

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    // ...
   })
 })
