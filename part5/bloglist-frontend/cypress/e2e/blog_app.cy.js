c759a225979614c69c2b9e6699dd1b38b7d759f6
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-22 23:15:53 +0800

    Update blog_app.cy.js

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,36 @@
 describe('Blog app', function() {
-  beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
-  })
+    // 5.17
+    beforeEach(function() {
+        cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        // create here a user to backend
+        cy.visit('http://localhost:3000')
+    })
 
-  it('Login form is shown', function() {
-    // ...
-  })
+    it('Login form is shown', function() {
+        // ...
+    })
+
+    // 5.18
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
+            // ...
+        })
+
+        it('fails with wrong credentials', function() {
+            // ...
+        })
+    })
+
+    // 5.19
+    describe('When logged in', function() {
+        beforeEach(function() {
+            // log in user here
+        })
+
+        it('A blog can be created', function() {
+            // ...
+        })
+    })
+
+
 })
