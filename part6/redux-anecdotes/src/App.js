43cbfad9a3e40156a7ff776f42209c1a3fb69df0
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-24 16:35:15 +0800

    refactor

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -5,18 +5,11 @@ import AnecdoteList from './components/AnecdoteList'
 const App = () => {
 
 
-
     return (
         <div>
             <h2>Anecdotes</h2>
-
-            <h2>create new</h2>
-            <form>
-                <div><input/></div>
-                <button>create</button>
-            </form>
-
-            <AnecdoteList />
+            <AnecdoteForm/>
+            <AnecdoteList/>
         </div>
     )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,11 @@
+const AnecdoteForm = () => {
+    return (
+        <div>
+            <form>
+                <div><input/></div>
+                <button>create</button>
+            </form>
+        </div>
+    )
+}
+export default AnecdoteForm
