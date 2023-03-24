1fc82fd1a07a292f60653ad3cbe00fe6b49ae41f
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-24 21:30:47 +0800

    Update index.js

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createStore} from 'redux'
+import {createStore, combineReducers} from 'redux'
 import {Provider} from 'react-redux'
 import App from './App'
+
 import reducer from './reducers/anecdoteReducer'
+import filterReducer from "./reducers/filterReducer";
 
 const store = createStore(reducer)
 
