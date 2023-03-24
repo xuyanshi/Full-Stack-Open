b41d4c4270879963f8bbef552d26de5fee253456
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-24 17:24:53 +0800

    Update AnecdoteForm.js

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,10 +1,14 @@
 import {useDispatch} from "react-redux";
+import {createAnecdote} from "../reducers/anecdoteReducer";
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
     const addAnecdote = (event) => {
         event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        dispatch(createAnecdote(content))
     }
 
     return (
