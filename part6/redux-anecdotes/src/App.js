e2eaabdffb7d6e74ffc50f4045ee31393ed3c7cc
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-24 16:36:43 +0800

    Update App.js

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -3,8 +3,6 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-
-
     return (
         <div>
             <h2>Anecdotes</h2>
