9276c7e6b934a1c5d849ca7742f5494d7b29343b
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-24 16:06:23 +0800

    Update anecdoteReducer.js

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,6 +23,9 @@ const reducer = (state = initialState, action) => {
     console.log('state now: ', state)
     console.log('action', action)
 
+    switch (action.type) {
+        case '':
+    }
     return state
 }
 
