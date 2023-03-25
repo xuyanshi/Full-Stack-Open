51a76265e2df6c1e2e24310b21513c4679de76e6
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-25 16:38:40 +0800

    fix the bug of filter

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,5 +1,10 @@
 const filterReducer = (state = 'ALL', action) => {
-    // ...
+    switch (action.type) {
+        case 'SET_FILTER':
+            return action.filter
+        default:
+            return state
+    }
 }
 
 export const filterChange = filter => {
