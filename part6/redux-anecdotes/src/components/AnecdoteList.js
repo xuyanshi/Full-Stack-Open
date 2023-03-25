96b9efe8b9d68141b8c077af9459f16e1f42530a
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-25 21:06:01 +0800

    Update AnecdoteList.js

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -12,7 +12,7 @@ const AnecdoteList = () => {
         }
         let filteredAnecdotes = []
         for (const anecdote of state.anecdotes) {
-            if (anecdote.content.indexOf(state.filterWord) !== -1) {
+            if (anecdote.content.search(state.filterWord) !== -1) {
                 filteredAnecdotes = filteredAnecdotes.concat(anecdote)
             }
         }
