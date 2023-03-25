5cdf2967c0b8b5eace52287f48c2b89b2494e462
Yanshi XU <xuyanshi1999@gmail.com>
2023-03-26 02:43:25 +0800

    part7-learning init

--- part7/part7-learning/src/index.js
+++ part7/part7-learning/src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Home = () => (
+    <div><h2>TKTL notes app</h2></div>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Notes = () => (
+    <div><h2>Notes</h2></div>
+)
+
+const Users = () => (
+    <div><h2>Users</h2></div>
+)
+
+const App = () => {
+    const [page, setPage] = useState('home')
+
+    const toPage = (page) => (event) => {
+        event.preventDefault()
+        setPage(page)
+    }
+
+    const content = () => {
+        if (page === 'home') {
+            return <Home/>
+        } else if (page === 'notes') {
+            return <Notes/>
+        } else if (page === 'users') {
+            return <Users/>
+        }
+    }
+
+    const padding = {
+        padding: 5
+    }
+
+    return (
+        <div>
+            <div>
+                <a href="" onClick={toPage('home')} style={padding}>
+                    home
+                </a>
+                <a href="" onClick={toPage('notes')} style={padding}>
+                    notes
+                </a>
+                <a href="" onClick={toPage('users')} style={padding}>
+                    users
+                </a>
+            </div>
+
+            {content()}
+        </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>, document.getElementById('root'))
