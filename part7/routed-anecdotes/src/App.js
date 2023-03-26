874d32c9a53bd8b2852a74a054f4f59bdd381385
xuyanshi <xuyanshi@ruc.edu.cn>
2023-03-26 18:16:31 +0800

    Update App.js

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -34,7 +34,10 @@ const AnecdoteList = ({anecdotes}) => (
     <div>
         <h2>Anecdotes</h2>
         <ul>
-            {anecdotes.map(anecdote => <li key={anecdote.id}>{anecdote.content}</li>)}
+            {anecdotes.map(anecdote => <li key={anecdote.id}>
+                    <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+                </li>
+            )}
         </ul>
     </div>
 )
